a387763481ce708bf176a2c3b5eb873830215afd
PatrickAsiedu <pattribette@gmail.com>
2023-07-19 16:23:56 +0000

    about to move

---
+++ src/middleware/credentials.ts
@@ -0,0 +1,12 @@
+import { Response, Request, NextFunction } from "express";
+import allowedOrigins from "../config/allowedOrigins";
+
+const credentails = (req: Request, res: Response, next: NextFunction) => {
+  const origin = req.header("Origin");
+  if (allowedOrigins.includes(origin as string)) {
+    res.header("Access-Control-Allow-Credentials");
+  }
+  next();
+};
+
+export default credentails;
--- src/server.ts
+++ src/server.ts
@@ -8,26 +8,34 @@ import connectDB from "./config/dbConn";
 import mongoose from "mongoose";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import cors from "cors";
+
 import * as Signup from "./routes/signup";
 import * as Login from "./routes/login";
 // import passport from "./config/passportLocal"
 import passport from "passport";
 import { passportConfig } from "./config/passportLocal";
 import isAuthenticated from "./middleware/auth";
+import CorsOptions from "./config/corsOptions";
+// import credentails from "./middleware/credentials";
 
 passportConfig(passport);
 
 const PORT = process.env.DB_PORT;
 connectDB();
 
+//middleware for logging
+app.use(morgan("dev"));
+
+// app.use(credentails)
+
+// app.use(cors(CorsOptions));
+
 // built-in middleware for json
 app.use(express.json());
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
-
-//middleware for logging
-app.use(morgan("dev"));
 
 app.use(
   session({
