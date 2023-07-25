922fd32a4a8dd94739dee2e16e58b970da413894
PatrickAsiedu <pattribette@gmail.com>
2023-07-25 12:00:27 +0000

    ts-node install

--- src/config/dbConn.ts
+++ src/config/dbConn.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 // import dotenv from "dotenv"
 // dotenv.config()
 
 const connectDB = async () => {
-    try {
-        mongoose.set("strictQuery", true)
-        await mongoose.connect(process.env.DB_URL)
-
-
-
-    } catch (error) {
-        console.error(error)
-
+  try {
+    mongoose.set("strictQuery", true);
+    if (process.env.DB_URL !== undefined) {
+      await mongoose.connect(process.env.DB_URL);
+    } else {
+      console.error("db url is undefined");
     }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-}
-
-export default connectDB
+export default connectDB;
