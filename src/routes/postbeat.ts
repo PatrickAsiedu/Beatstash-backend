5d95e0592a7ea981be91f2ce8f46d59a923ba29c
PatrickAsiedu <pattribette@gmail.com>
2024-03-15 20:17:51 +0000

    tested formdata

--- src/routes/postbeat.ts
+++ src/routes/postbeat.ts
@@ -5,6 +5,6 @@ import multer from "multer";
 const router = Router();
 const upload = multer();
 
-router.post("/", upload.single("beat"), postBeatController.handlePostBeat);
+router.post("/", upload.array("files"), postBeatController.handlePostBeat);
 
 export { router };
