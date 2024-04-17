3f2d6fdc4989eec3a32df1621db891ca2f05d23f
PatrickAsiedu <pattribette@gmail.com>
2024-04-17 12:09:29 +0000

    first deployment

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -5,7 +5,7 @@ type origin = string | undefined;
 const CorsOptions = {
   origin: (origin: origin, callback: any) => {
     if (origin !== undefined) {
-      if (allowedOrigins.indexOf(origin) !== -1) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
