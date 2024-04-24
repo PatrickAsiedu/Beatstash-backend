dec9f571419f219130fe6b44fa0c08d5f1a91e86
PatrickAsiedu <pattribette@gmail.com>
2024-04-24 18:13:50 +0000

    Revert "added dist"
    This reverts commit 3cd649b4d4c744c58545886d2862c233bf46d103.

--- dist/middleware/filesExists.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fileExists = (req, res, next) => {
-    const submittedFiles = req.files;
-    if (Object.values(submittedFiles).length === 0)
-        return res.status(400).json({ message: "Missing files" });
-    next();
-};
-exports.default = fileExists;
